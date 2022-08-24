f91b0c3a11696a44ef3b1cf98510ccb87a1e8daf
du33169 <du33169@qq.com>
2022-08-24 22:28:57 +0800

    feat: switch to fit_predict

--- nvkmeans.py
+++ nvkmeans.py
@@ -1,33 +1,32 @@
 from ctypes import *
-import numpy as torch
+import numpy as np
 import torch
 from numpy.ctypeslib import as_ctypes
+from sklearn.cluster import kmeans_plusplus
 nvkmeans = CDLL("libnvkmeans.so")
 class NVKmeans:
 
 	def __init__(self,k) -> None:
 		self.k=k
 
-	def fit(self,X:torch.Tensor,maxIter)-> int: # return the number of actual iterations
-		n,d= (X.shape[0]), (X.shape[1])
-		centers=torch.empty((n*d,1),dtype=torch.int32)
-		labels=torch.empty((n,1),dtype=torch.int32)
-		#ramdom generate center
-		iter=nvkmeans.fit(
+	def fit_predict(self,X:torch.Tensor,maxIter)-> torch.Tensor: # return labels
+		n,d= X.shape
+		# using kmeans++ to select initial centers
+		print('[py]kmeans++...')
+		centers, indices = kmeans_plusplus(X.numpy(), n_clusters=self.k, random_state=0)
+		centers=centers.ravel()
+		centers=centers.astype(np.double)
+		labels=np.empty((n,1),dtype=np.int32)
+
+		print('[py]invoking clib...')
+		self.iter=nvkmeans.fit(
 			c_int(maxIter),c_int(n),c_int(d),c_int(self.k),
-			as_ctypes(X.ravel()),
+			as_ctypes(X.numpy().ravel()),
 			as_ctypes(centers),
 			as_ctypes(labels),
 			)
-		centers.reshape((n,d))
-		self.centers=centers
-		return iter
-
-	def predict(self,X:torch.Tensor):
-		eudis=lambda x,y:torch.linalg.norm(x-y)
-		def nearest(x)->int:
-			'''return nearest label'''
-			dises=torch.tensor([eudis(x,c) for c in self.centers])
-			return torch.argmin(dises)
-		return torch.tensor([nearest(x) for x in X])
+		centers.reshape((self.k,d))
+		self.centers=torch.from_numpy(centers)
+		print('[py]done')
+		return torch.from_numpy(labels)
 
