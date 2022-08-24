c649c131c3be6cb46e4d253454ec5cebc7e3be25
du33169 <du33169@qq.com>
2022-08-24 16:43:08 +0800

    rename cpp to cu

--- nvkmeans.cu
+++ nvkmeans.cu
@@ -1,5 +1,4 @@
 //nvkmeans
-#include "nvkmeans.h"
 
 #include <thrust/device_vector.h>
 #include "kmeans.h"
@@ -14,7 +13,7 @@ void random_labels(thrust::device_vector<int>& labels, int n, int k) {
 
 extern "C"
 {
-int fit(
+ __attribute__((visibility ("default"))) int fit(
 	//input
 	int maxIter, int n, int d, int k, double *dataset, 
 	
@@ -38,17 +37,18 @@ int fit(
 	random_labels(*labels_v[0], n, k);
 	bool init_from_labels = true;
 	double threshold = 1e-4;
-	int iter = kmeans::kmeans(iterations, n, d, k, data_v, labels_v, centroids_v, distances_v, n_gpu, init_from_labels,1e-4);
+	int n_gpu=1;
+	int iter = kmeans::kmeans(maxIter, n, d, k, data_v, labels_v, centroids_v, distances_v, n_gpu, init_from_labels,threshold);
 
 	int* labels_dv= thrust::raw_pointer_cast(labels_v[0]->data());
 	cudaMemcpy(labels, labels_dv, n * sizeof(int), cudaMemcpyDeviceToHost);
 	double* centers_dv= thrust::raw_pointer_cast(centroids_v[0]->data());
 	cudaMemcpy(centers, centers_dv, k*d * sizeof(int), cudaMemcpyDeviceToHost);
 	//clean
-	delete (data[0]);
-	delete(labels[0]);
-    delete(centroids[0]);
-    delete(distances[0]);
+	delete (data_v[0]);
+	delete(labels_v[0]);
+    delete(centroids_v[0]);
+    delete(distances_v[0]);
 	return iter;
 }
 
