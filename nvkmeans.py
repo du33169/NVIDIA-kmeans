dfcd593aa6db6f0a3742d307d66e4d9c5e7ed20b
du33169 <du33169@qq.com>
2022-08-26 14:51:06 +0800

    feat: add init mode support

--- nvkmeans.py
+++ nvkmeans.py
@@ -1,19 +1,33 @@
 from ctypes import *
 import numpy as np
+import numpy.random
 import torch
 from numpy.ctypeslib import as_ctypes
 from sklearn.cluster import kmeans_plusplus
 nvkmeans = CDLL("libnvkmeans.so")
 class NVKmeans:
 
-	def __init__(self,k) -> None:
+	def __init__(self,k,init) -> None:
 		self.k=k
+		self.init=init
+
+	def init_centers(self,X)->np.ndarray:
+		X=X.numpy()
+		if self.init=='k-means++':
+			centers, indices = kmeans_plusplus(X, n_clusters=self.k, random_state=0)
+		elif self.init=='random':
+			ind=np.random.randint(X.shape[0],size=self.k)
+			centers=X[ind]
+		else:
+			assert False, f"[NVKmeans] unhandled init type, expected 'k-means++' or 'random', got {self.init} "
+		return centers
 
 	def fit_predict(self,X:torch.Tensor,maxIter)-> torch.Tensor: # return labels
+		print(X.shape)
 		n,d= X.shape
 		# using kmeans++ to select initial centers
 		print('[py]kmeans++...')
-		centers, indices = kmeans_plusplus(X.numpy(), n_clusters=self.k, random_state=0)
+		centers=self.init_centers(X)
 		centers=centers.ravel()
 		centers=centers.astype(np.double)
 		labels=np.empty(n,dtype=np.int32)
